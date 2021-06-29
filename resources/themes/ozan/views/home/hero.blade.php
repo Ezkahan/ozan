7c6f06206fbe847dec1d4808054f99a4b31498dd
=alynurly <=alynurly@gmail.com>
2021-06-29 08:02:06 +0500

    font-fix

--- resources/themes/ozan/views/home/hero.blade.php
+++ resources/themes/ozan/views/home/hero.blade.php
@@ -19,14 +19,14 @@ foreach (app('Webkul\Category\Repositories\CategoryRepository')->getVisibleCateg
                     @foreach ($categories as $category)
                         <div class="card" style="cursor: pointer;border-radius: 0;" data-toggle="collapse" data-target="#faqCollapse-{{ $loop->iteration }}" data-aria-expanded="true" data-aria-controls="faqCollapse-{{ $loop->iteration }}">
                             <div class="hero__sidebar-inner-link" id="faqHeading-{{ $loop->iteration }}">
-                                <i class="icon-{{ $category['icon'] }}"></i>
+                                <i class="{{ $category['icon'] }}"></i>
                                 <span class="faq-title"> {{ $category['name'] }}</span>
                             </div>
                             <div id="faqCollapse-{{ $loop->iteration }}" class="collapse" aria-labelledby="faqHeading-{{ $loop->iteration }}" data-callback="console.log(true);" data-parent="#accordion">
                                 <div class="card-body" style="padding: 10px;">
                                     @foreach($category->children as $child)
                                         <a href="{{ $child['url_path'] }}" class="hero__sidebar-inner-link ">
-                                            <i class="icon-{{ $child['icon'] }}"></i>
+                                            <i class="{{ $child['icon'] }}"></i>
                                             <span>{{$child['name']}}</span>
                                         </a>
                                     @endforeach
