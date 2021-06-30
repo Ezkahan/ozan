c9f388f92fac15e822f6e180be775a4c691cd7bc
=alynurly <=alynurly@gmail.com>
2021-06-30 18:01:00 +0500

    added another banner slider

--- resources/themes/ozan/views/home/hero.blade.php
+++ resources/themes/ozan/views/home/hero.blade.php
@@ -1,4 +1,3 @@
-
 <?php
 
 $categories = [];
@@ -17,24 +16,24 @@ foreach (app('Webkul\Category\Repositories\CategoryRepository')->getVisibleCateg
             <div class="hero__sidebar">
                 <div class="hero__sidebar-inner faq" id="accordion">
                     @foreach ($categories as $category)
-                        <div class="card" style="cursor: pointer;border-radius: 0;" data-toggle="collapse" data-target="#faqCollapse-{{ $loop->iteration }}" data-aria-expanded="true" data-aria-controls="faqCollapse-{{ $loop->iteration }}">
-                            <div class="hero__sidebar-inner-link" id="faqHeading-{{ $loop->iteration }}">
-                                <i class="{{ $category['icon'] }}">
-                                    <span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span><span class="path8"></span><span class="path9"></span><span class="path10"></span><span class="path11"></span><span class="path12"></span><span class="path13"></span><span class="path14"></span><span class="path15"></span><span class="path16"></span><span class="path17"></span><span class="path18"></span><span class="path19"></span></span>
-                                </i>
-                                <span class="faq-title"> {{ $category['name'] }}</span>
-                            </div>
-                            <div id="faqCollapse-{{ $loop->iteration }}" class="collapse" aria-labelledby="faqHeading-{{ $loop->iteration }}" data-callback="console.log(true);" data-parent="#accordion">
-                                <div class="card-body" style="padding: 10px;">
-                                    @foreach($category->children as $child)
-                                        <a href="{{ $child['url_path'] }}" class="hero__sidebar-inner-link ">
-                                            <i class="icon-{{ $child['icon'] }}"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span><span class="path8"></span><span class="path9"></span><span class="path10"></span><span class="path11"></span><span class="path12"></span><span class="path13"></span><span class="path14"></span><span class="path15"></span><span class="path16"></span><span class="path17"></span><span class="path18"></span><span class="path19"></span></span></i>
-                                            <span>{{$child['name']}}</span>
-                                        </a>
-                                    @endforeach
-                                </div>
+                    <div class="card" style="cursor: pointer;border-radius: 0;" data-toggle="collapse" data-target="#faqCollapse-{{ $loop->iteration }}" data-aria-expanded="true" data-aria-controls="faqCollapse-{{ $loop->iteration }}">
+                        <div class="hero__sidebar-inner-link" id="faqHeading-{{ $loop->iteration }}">
+                            <i class="{{ $category['icon'] }}">
+                                <span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span><span class="path8"></span><span class="path9"></span><span class="path10"></span><span class="path11"></span><span class="path12"></span><span class="path13"></span><span class="path14"></span><span class="path15"></span><span class="path16"></span><span class="path17"></span><span class="path18"></span><span class="path19"></span></span>
+                            </i>
+                            <span class="faq-title"> {{ $category['name'] }}</span>
+                        </div>
+                        <div id="faqCollapse-{{ $loop->iteration }}" class="collapse" aria-labelledby="faqHeading-{{ $loop->iteration }}" data-callback="console.log(true);" data-parent="#accordion">
+                            <div class="card-body" style="padding: 10px;">
+                                @foreach($category->children as $child)
+                                <a href="{{ $child['url_path'] }}" class="hero__sidebar-inner-link ">
+                                    <i class="icon-{{ $child['icon'] }}"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span><span class="path8"></span><span class="path9"></span><span class="path10"></span><span class="path11"></span><span class="path12"></span><span class="path13"></span><span class="path14"></span><span class="path15"></span><span class="path16"></span><span class="path17"></span><span class="path18"></span><span class="path19"></span></span></i>
+                                    <span>{{$child['name']}}</span>
+                                </a>
+                                @endforeach
                             </div>
                         </div>
+                    </div>
                     @endforeach
                 </div>
             </div>
@@ -46,7 +45,7 @@ foreach (app('Webkul\Category\Repositories\CategoryRepository')->getVisibleCateg
                     <i class="icon-chevron-right"></i>
                 </div>
                 @include('shop::home.slider',['sliderData' => $sliderData])
-               
+
             </div>
         </div>
     </div>
