68267e00486b33bcc648047c46f4e143bc59175c
=alynurly <=alynurly@gmail.com>
2021-06-30 09:29:33 +0500

    related products

--- packages/Webkul/Shop/src/Resources/lang/tm/app.php
+++ packages/Webkul/Shop/src/Resources/lang/tm/app.php
@@ -461,7 +461,7 @@ return [
         'total-rating' => ':total_rating Ratings & :total_reviews Reviews',
         'by' => 'By :name',
         'up-sell-title' => 'We found other products you might like!',
-        'related-product-title' => 'Related Products',
+        'related-product-title' => 'Degişli önümler',
         'cross-sell-title' => 'More choices',
         'reviews-title' => 'Ratings & Reviews',
         'write-review-btn' => 'Write Review',
