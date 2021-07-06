2c3f27ee27cd83a9aa2b16cc68a8655e8246541e
merdan <merdan.m@gmail.com>
2021-07-06 23:09:05 +0500

    akhasap category and icon bug

--- resources/themes/ozan/views/home/hero.blade.php
+++ resources/themes/ozan/views/home/hero.blade.php
@@ -3,7 +3,7 @@
 $categories = [];
 
 
-foreach (app('Webkul\Category\Repositories\CategoryRepository')->getVisibleCategoryTree(core()->getCurrentChannel()->root_category_id) as $category) {
+foreach (app('Webkul\Category\Repositories\CategoryRepository')->getCategoryTree(core()->getCurrentChannel()->root_category_id) as $category) {
     if ($category->slug) {
         array_push($categories, $category);
     }
@@ -16,17 +16,17 @@ foreach (app('Webkul\Category\Repositories\CategoryRepository')->getVisibleCateg
             <div class="hero__sidebar">
                 <div class="hero__sidebar-inner faq" id="accordion">
                     @foreach ($categories as $category)
-                    <div class="card" style="cursor: pointer;border-radius: 0;" data-toggle="collapse" data-target="#faqCollapse-{{ $loop->iteration }}" data-aria-expanded="true" data-aria-controls="faqCollapse-{{ $loop->iteration }}">
+                    <div class="card" style="cursor: pointer;border-radius: 0; " @if($category['status']) data-toggle="collapse" @endif data-target="#faqCollapse-{{ $loop->iteration }}" data-aria-expanded="true" data-aria-controls="faqCollapse-{{ $loop->iteration }}">
                         <div class="hero__sidebar-inner-link" id="faqHeading-{{ $loop->iteration }}">
                             <i class="{{ $category['icon'] }}">
                                 <span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span><span class="path8"></span><span class="path9"></span><span class="path10"></span><span class="path11"></span><span class="path12"></span><span class="path13"></span><span class="path14"></span><span class="path15"></span><span class="path16"></span><span class="path17"></span><span class="path18"></span><span class="path19"></span>
                             </i>
-                            <span class="faq-title"> {{ $category['name'] }}</span>
+                            <span class="faq-title @if(!$category['status']) text-muted @endif"> {{ $category['name'] }}</span>
                         </div>
                         <div id="faqCollapse-{{ $loop->iteration }}" class="collapse" aria-labelledby="faqHeading-{{ $loop->iteration }}" data-callback="console.log(true);" data-parent="#accordion">
                             <div class="card-body" style="padding: 10px;">
                                 @foreach($category->children as $child)
-                                <a href="{{ $child['url_path'] }}" class="hero__sidebar-inner-link ">
+                                <a href="@if($child['status']){{ $child['url_path'] }}@else # @endif" class="hero__sidebar-inner-link " disabled>
                                     <i class="icon-{{ $child['icon'] }}"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span><span class="path8"></span><span class="path9"></span><span class="path10"></span><span class="path11"></span><span class="path12"></span><span class="path13"></span><span class="path14"></span><span class="path15"></span><span class="path16"></span><span class="path17"></span><span class="path18"></span><span class="path19"></span></i>
                                     <span>{{$child['name']}}</span>
                                 </a>
