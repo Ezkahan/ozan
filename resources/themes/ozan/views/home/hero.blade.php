d93a63dddf14ff327fd9c701c49f9323e931e6bf
merdan <merdan.m@gmail.com>
2021-07-07 15:13:18 +0500

    category icon

--- resources/themes/ozan/views/home/hero.blade.php
+++ resources/themes/ozan/views/home/hero.blade.php
@@ -18,7 +18,7 @@ foreach (app('Webkul\Category\Repositories\CategoryRepository')->getCategoryTree
                     @foreach ($categories as $category)
                     <div class="card" style="cursor: pointer;border-radius: 0; " @if($category['status']) data-toggle="collapse" @endif data-target="#faqCollapse-{{ $loop->iteration }}" data-aria-expanded="true" data-aria-controls="faqCollapse-{{ $loop->iteration }}">
                         <div class="hero__sidebar-inner-link" id="faqHeading-{{ $loop->iteration }}">
-                            <i class="{{ $category['icon'] }}">
+                            <i class="icon-{{ $category['icon'] }}">
                                 <span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span><span class="path8"></span><span class="path9"></span><span class="path10"></span><span class="path11"></span><span class="path12"></span><span class="path13"></span><span class="path14"></span><span class="path15"></span><span class="path16"></span><span class="path17"></span><span class="path18"></span><span class="path19"></span>
                             </i>
                             <span class="faq-title @if(!$category['status']) text-muted @endif"> {{ $category['name'] }}</span>
