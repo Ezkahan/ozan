825fa4838ec20d329f3a27e39f46468000877245
saparatayev <sopa.97@mail.ru>
2021-07-09 21:53:36 +0500

    lang change

--- resources/themes/velocity_ozan/views/layouts/top-nav/index.blade.php
+++ resources/themes/velocity_ozan/views/layouts/top-nav/index.blade.php
@@ -7,3 +7,51 @@
         @include('velocity::layouts.top-nav.login-section')
     </div>
 </nav>
+
+<section class="upheader">
+    <div class="auto__container">
+        <div class="upheader__inner">
+            <div class="upheader__language">
+                <i class="icon-global"></i>
+                <select 
+                    name="language"
+                    onchange="window.location.href = this.value"
+                    aria-label="Locale"
+                    @if (count(core()->getCurrentChannel()->locales) == 1)
+                        disabled="disabled"
+                    @endif>
+
+                    @foreach (core()->getCurrentChannel()->locales as $locale)
+                        @if (isset($searchQuery) && $searchQuery)
+                            <option 
+                                value="?{{ $searchQuery }}&locale={{ $locale->code }}"
+                                {{ $locale->code == app()->getLocale() ? 'selected' : '' }}>
+                                {{ $locale->name }}
+                            </option>
+                        @else
+                            <option value="?locale={{ $locale->code }}" {{ $locale->code == app()->getLocale() ? 'selected' : '' }}>{{ $locale->name }}</option>
+                        @endif
+                    @endforeach
+                </select>
+            </div>
+            <div class="upheader__nav">
+                <a href="about.html" class="upheader__nav-link">
+                    <i class="icon-info"></i>
+                    <span>О нас</span>
+                </a>
+                <a href="delivery.html" class="upheader__nav-link">
+                    <i class="icon-box"></i>
+                    <span>Доставка</span>
+                </a>
+                <a href="#" class="upheader__nav-link">
+                    <i class="icon-money"></i>
+                    <span>Методы оплаты</span>
+                </a>
+                <a href="#" class="upheader__nav-link">
+                    <i class="icon-help"></i>
+                    <span>Поддержка</span>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
