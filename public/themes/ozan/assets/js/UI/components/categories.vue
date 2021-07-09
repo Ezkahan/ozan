8d4f7edc2af8795fbfd864b0c63d836a22de4846
Meylis Gazakow <Meylis7@users.noreply.github.com>
2021-07-09 10:35:24 +0300

    mycommit2

--- public/themes/ozan/assets/js/UI/components/categories.vue
+++ public/themes/ozan/assets/js/UI/components/categories.vue
@@ -1,34 +1,57 @@
 <template>
-    <div class="hero__sidebar-inner faq" id="accordion">
-    
-        <vsa-list :tags="my_options.tags">
-  
-            <vsa-item v-for="(category, index) in categories" :key="index" class="card" style="cursor: pointer;border-radius: 0;">
-                
-                    <vsa-heading>
-                        <i :class="category.icon">
-                            <span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span><span class="path8"></span><span class="path9"></span><span class="path10"></span><span class="path11"></span><span class="path12"></span><span class="path13"></span><span class="path14"></span><span class="path15"></span><span class="path16"></span><span class="path17"></span><span class="path18"></span><span class="path19"></span>
-                        </i>
-                        <span class="faq-title"> {{ category.name }}</span>
-                    </vsa-heading>
-                
+  <div class="hero__sidebar-inner faq" id="accordion">
+    <vsa-list :tags="my_options.tags">
+      <vsa-item
+        v-for="(category, index) in categories"
+        :key="index"
+        class="card"
+        style="cursor: pointer; border-radius: 0"
+      >
+        <vsa-heading>
+          <i :class="category.icon">
+            <span class="path1"></span><span class="path2"></span
+            ><span class="path3"></span><span class="path4"></span
+            ><span class="path5"></span><span class="path6"></span
+            ><span class="path7"></span><span class="path8"></span
+            ><span class="path9"></span><span class="path10"></span
+            ><span class="path11"></span><span class="path12"></span
+            ><span class="path13"></span><span class="path14"></span
+            ><span class="path15"></span><span class="path16"></span
+            ><span class="path17"></span><span class="path18"></span
+            ><span class="path19"></span>
+          </i>
+          <span class="faq-title"> {{ category.name }}</span>
+        </vsa-heading>
 
-                <vsa-content>
-                    <div class="card-body" style="padding: 10px;">
-                        <!-- @foreach($category->children as $child) -->
-                        <a v-for="(child, index) in category.children" :key="index" :href="child.url_path" class="hero__sidebar-inner-link ">
-                            <i :class="'icon-' + child.icon"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span><span class="path8"></span><span class="path9"></span><span class="path10"></span><span class="path11"></span><span class="path12"></span><span class="path13"></span><span class="path14"></span><span class="path15"></span><span class="path16"></span><span class="path17"></span><span class="path18"></span><span class="path19"></span></i>
-                            <span>{{child.name}}</span>
-                        </a>
-                        <!-- @endforeach -->
-                    </div>
-                </vsa-content>
-            </vsa-item>
-            
-        </vsa-list>
-
-    
-    </div>
+        <vsa-content>
+          <div class="card-body" style="padding: 10px">
+            <!-- @foreach($category->children as $child) -->
+            <a
+              v-for="(child, index) in category.children"
+              :key="index"
+              :href="child.url_path"
+              class="hero__sidebar-inner-link"
+            >
+              <i :class="'icon-' + child.icon"
+                ><span class="path1"></span><span class="path2"></span
+                ><span class="path3"></span><span class="path4"></span
+                ><span class="path5"></span><span class="path6"></span
+                ><span class="path7"></span><span class="path8"></span
+                ><span class="path9"></span><span class="path10"></span
+                ><span class="path11"></span><span class="path12"></span
+                ><span class="path13"></span><span class="path14"></span
+                ><span class="path15"></span><span class="path16"></span
+                ><span class="path17"></span><span class="path18"></span
+                ><span class="path19"></span
+              ></i>
+              <span>{{ child.name }}</span>
+            </a>
+            <!-- @endforeach -->
+          </div>
+        </vsa-content>
+      </vsa-item>
+    </vsa-list>
+  </div>
 </template>
 
 <script>
@@ -37,51 +60,51 @@ import {
   VsaItem,
   VsaHeading,
   VsaContent,
-  VsaIcon
-} from 'vue-simple-accordion';
+  VsaIcon,
+} from "vue-simple-accordion";
 
 // import 'vue-simple-accordion/dist/vue-simple-accordion.css';
 
 export default {
-    name: "categories",
-    components: {
-        VsaList,
-        VsaItem,
-        VsaHeading,
-        VsaContent,
-        VsaIcon
+  name: "categories",
+  components: {
+    VsaList,
+    VsaItem,
+    VsaHeading,
+    VsaContent,
+    VsaIcon,
+  },
+  props: {
+    categories: {
+      type: Array,
+      required: true,
+      default: () => [],
     },
-    props: {
-        categories: {
-            type: Array,
-            required: true,
-            default: () => [],
+  },
+  data() {
+    return {
+      my_options: {
+        tags: {
+          list: "div",
+          list__item: "div",
+          item__heading: "div",
+          heading__content: "span",
+          heading__icon: "span",
+          item__content: "div",
         },
-    },
-    data () {
-        return {
-            my_options: {
-                tags: {
-                    list: "div",
-                    list__item: "div",
-                    item__heading: "div",
-                    heading__content: "span",
-                    heading__icon: "span",
-                    item__content: "div"
-                },
-                roles: {
-                    presentation: false,
-                    heading: false,
-                    region: true
-                },
-                transition: "vsa-collapse",
-                initActive: false,
-                forceActive: undefined,
-                autoCollapse: true,
-                onHeadingClick: () => {},
-                navigation: true
-            }
-        }
-    }
-}
+        roles: {
+          presentation: false,
+          heading: false,
+          region: true,
+        },
+        transition: "vsa-collapse",
+        initActive: false,
+        forceActive: undefined,
+        autoCollapse: true,
+        onHeadingClick: () => {},
+        navigation: true,
+      },
+    };
+  },
+};
 </script>
