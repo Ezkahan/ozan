5603d970625ca5ceebd06913b98cf4e104fb868d
merdan <merdan.m@gmail.com>
2021-07-16 14:38:54 +0500

    Diller turkiman

--- resources/themes/velocity_ozan/views/customers/signup/index.blade.php
+++ resources/themes/velocity_ozan/views/customers/signup/index.blade.php
@@ -1,7 +1,7 @@
 @extends('shop::layouts.master')
 
 @section('page_title')
-    {{ __('shop::app.customer.signup-form.page-title') }}
+    {{ __('velocity::app.customer.signup-form.page-title') }}
 @endsection
 
 @section('full-content-wrapper')
@@ -42,7 +42,7 @@
 
                         <div class="control-group" :class="[errors.has('first_name') ? 'has-error' : '']">
                             <label for="first_name" class="required label-style">
-                                {{ __('shop::app.customer.signup-form.firstname') }}
+                                {{ __('velocity::app.customer.signup-form.firstname') }}
                             </label>
 
                             <input
@@ -51,7 +51,7 @@
                                 name="first_name"
                                 v-validate="'required'"
                                 value="{{ old('first_name') }}"
-                                data-vv-as="&quot;{{ __('shop::app.customer.signup-form.firstname') }}&quot;" />
+                                data-vv-as="&quot;{{ __('velocity::app.customer.signup-form.firstname') }}&quot;" />
 
                             <span class="control-error" v-if="errors.has('first_name')">
                                 @{{ errors.first('first_name') }}
@@ -62,7 +62,7 @@
 
                         <div class="control-group" :class="[errors.has('last_name') ? 'has-error' : '']">
                             <label for="last_name" class="required label-style">
-                                {{ __('shop::app.customer.signup-form.lastname') }}
+                                {{ __('velocity::app.customer.signup-form.lastname') }}
                             </label>
 
                             <input
@@ -71,7 +71,7 @@
                                 name="last_name"
                                 v-validate="'required'"
                                 value="{{ old('last_name') }}"
-                                data-vv-as="&quot;{{ __('shop::app.customer.signup-form.lastname') }}&quot;" />
+                                data-vv-as="&quot;{{ __('velocity::app.customer.signup-form.lastname') }}&quot;" />
 
                             <span class="control-error" v-if="errors.has('last_name')">
                                 @{{ errors.first('last_name') }}
@@ -83,7 +83,7 @@
                         <div class="input-group" :class="[errors.has('phone') ? 'has-error' : '']">
 
                             <label for="phone" class="required label-style">
-                                {{ __('shop::app.registerlogin.phoneNumber') }}
+                                {{ __('velocity::app.customer.signup-form.phone') }}
                             </label>
                             <div class="input-group-prepend">
                                 <span class="input-group-text" id="basic-addon1">+993</span>
@@ -91,7 +91,7 @@
                             <input
                                 type="tel"
 
-                                class="form-control form-style"
+                                class="form-control pl-2"
                                 name="phone"
                                 v-validate="'required|numeric|digits:8'"
                                 value="{{ old('phone') }}"
@@ -106,7 +106,7 @@
 
                         <div class="control-group" :class="[errors.has('password') ? 'has-error' : '']">
                             <label for="password" class="required label-style">
-                                {{ __('shop::app.customer.signup-form.password') }}
+                                {{ __('velocity::app.customer.signup-form.password') }}
                             </label>
 
                             <input
@@ -116,7 +116,7 @@
                                 v-validate="'required|min:6'"
                                 ref="password"
                                 value="{{ old('password') }}"
-                                data-vv-as="&quot;{{ __('shop::app.customer.signup-form.password') }}&quot;" />
+                                data-vv-as="&quot;{{ __('velocity::app.customer.signup-form.password') }}&quot;" />
 
                             <span class="control-error" v-if="errors.has('password')">
                                 @{{ errors.first('password') }}
@@ -127,7 +127,7 @@
 
                         <div class="control-group" :class="[errors.has('password_confirmation') ? 'has-error' : '']">
                             <label for="password_confirmation" class="required label-style">
-                                {{ __('shop::app.customer.signup-form.confirm_pass') }}
+                                {{ __('velocity::app.customer.signup-form.confirm_pass') }}
                             </label>
 
                             <input
@@ -135,7 +135,7 @@
                                 class="form-style"
                                 name="password_confirmation"
                                 v-validate="'required|min:6|confirmed:password'"
-                                data-vv-as="&quot;{{ __('shop::app.customer.signup-form.confirm_pass') }}&quot;" />
+                                data-vv-as="&quot;{{ __('velocity::app.customer.signup-form.confirm_pass') }}&quot;" />
 
                             <span class="control-error" v-if="errors.has('password_confirmation')">
                                 @{{ errors.first('password_confirmation') }}
@@ -145,14 +145,14 @@
                         @if (core()->getConfigData('customer.settings.newsletter.subscription'))
                             <div class="control-group">
                                 <input type="checkbox" id="checkbox2" name="is_subscribed">
-                                <span>{{ __('shop::app.customer.signup-form.subscribe-to-newsletter') }}</span>
+                                <span>{{ __('velocity::app.customer.signup-form.subscribe-to-newsletter') }}</span>
                             </div>
                         @endif
 
                         {!! view_render_event('bagisto.shop.customers.signup_form_controls.after') !!}
 
                         <button class="theme-btn" type="submit">
-                            {{ __('shop::app.customer.signup-form.title') }}
+                            {{ __('velocity::app.customer.signup-form.title') }}
                         </button>
                     </form>
 
