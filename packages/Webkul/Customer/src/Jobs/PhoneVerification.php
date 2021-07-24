d2e7c244a8e3bde965709c31e8cffc56c2576de1
merdan <merdan.m@gmail.com>
2021-07-24 20:28:41 +0500

    gitignore

--- packages/Webkul/Customer/src/Jobs/PhoneVerification.php
+++ packages/Webkul/Customer/src/Jobs/PhoneVerification.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Http;
+use Throwable;
 use Webkul\Customer\SMS\VerificationSMS;
 
 class PhoneVerification implements ShouldQueue
