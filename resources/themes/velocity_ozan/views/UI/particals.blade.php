226d01db717c46df9c6b9d690a0ad212bd8b5693
merdan <merdan.m@gmail.com>
2021-08-16 12:26:29 +0500

    breadcrumbs1

--- resources/themes/velocity_ozan/views/UI/particals.blade.php
+++ resources/themes/velocity_ozan/views/UI/particals.blade.php
@@ -109,7 +109,6 @@
                                         aria-label="Search"
                                         v-model:value="inputVal" />
 
-                                    <image-search-component></image-search-component>
 
                                     <button class="search__btn" type="button"  aria-label="Search" @click="submitForm">
                                         @lang('app.search')
@@ -172,28 +171,6 @@
         </div>
     </script>
 
-    <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@tensorflow-models/mobilenet"></script>
-
-    <script type="text/x-template" id="image-search-component-template">
-        <div class="d-inline-block image-search-container" v-if="image_search_status">
-            <label for="image-search-container">
-                <i class="icon camera-icon"></i>
-
-                <input
-                    type="file"
-                    class="d-none"
-                    ref="image_search_input"
-                    id="image-search-container"
-                    v-on:change="uploadImage()" />
-
-                <img
-                    class="d-none"
-                    id="uploaded-image-url"
-                    :src="uploadedImageUrl" alt="" width="20" height="20" />
-            </label>
-        </div>
-    </script>
 
     <script type="text/javascript">
         (() => {
@@ -368,110 +345,6 @@
                 }
             });
 
-            Vue.component('image-search-component', {
-                template: '#image-search-component-template',
-                data: function() {
-                    return {
-                        uploadedImageUrl: '',
-                        image_search_status: "{{core()->getConfigData('general.content.shop.image_search') == '1' ? 'true' : 'false'}}" == 'true'
-                    }
-                },
-
-                methods: {
-                    uploadImage: function() {
-                        var imageInput = this.$refs.image_search_input;
-
-                        if (imageInput.files && imageInput.files[0]) {
-                            if (imageInput.files[0].type.includes('image/')) {
-                                if (imageInput.files[0].size <= 2000000) {
-                                    this.$root.showLoader();
-
-                                    var formData = new FormData();
-
-                                    formData.append('image', imageInput.files[0]);
-
-                                    axios.post(
-                                        "{{ route('shop.image.search.upload') }}",
-                                        formData,
-                                        {
-                                            headers: {
-                                                'Content-Type': 'multipart/form-data'
-                                            }
-                                        }
-                                    ).then(response => {
-                                        var net;
-                                        var self = this;
-                                        this.uploadedImageUrl = response.data;
-
-
-                                        async function app() {
-                                            var analysedResult = [];
-
-                                            var queryString = '';
-
-                                            net = await mobilenet.load();
-
-                                            const imgElement = document.getElementById('uploaded-image-url');
-
-                                            try {
-                                                const result = await net.classify(imgElement);
-
-                                                result.forEach(function(value) {
-                                                    queryString = value.className.split(',');
-
-                                                    if (queryString.length > 1) {
-                                                        analysedResult = analysedResult.concat(queryString)
-                                                    } else {
-                                                        analysedResult.push(queryString[0])
-                                                    }
-                                                });
-                                            } catch (error) {
-                                                self.$root.hideLoader();
-
-                                                window.showAlert(
-                                                    `alert-danger`,
-                                                    this.__('shop.general.alert.error'),
-                                                    "{{ __('shop::app.common.error') }}"
-                                                );
-                                            }
-
-                                            localStorage.searchedImageUrl = self.uploadedImageUrl;
-
-                                            queryString = localStorage.searched_terms = analysedResult.join('_');
-
-                                            self.$root.hideLoader();
-
-                                            window.location.href = "{{ route('shop.search.index') }}" + '?term=' + queryString + '&image-search=1';
-                                        }
-
-                                        app();
-                                    }).catch(() => {
-                                        this.$root.hideLoader();
-
-                                        window.showAlert(
-                                            `alert-danger`,
-                                            this.__('shop.general.alert.error'),
-                                            "{{ __('shop::app.common.error') }}"
-                                        );
-                                    });
-                                } else {
-                                        imageInput.value = '';
-
-                                        window.showAlert(
-                                            `alert-danger`,
-                                            this.__('shop.general.alert.error'),
-                                            "{{ __('shop::app.common.image-upload-limit') }}"
-                                        );
-                                }
-                            } else {
-                                imageInput.value = '';
-
-                                alert('Only images (.jpeg, .jpg, .png, ..) are allowed.');
-                            }
-                        }
-                    }
-                }
-            });
         })()
     </script>
 @endpush
