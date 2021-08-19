2069ad03c3138277c9a033de974fc2532024fe79
merdan <merdan.m@gmail.com>
2021-08-19 11:16:08 +0500

    push notfication logging fx9

--- packages/Webkul/Admin/src/SMS/AcceptOrderSMS.php
+++ packages/Webkul/Admin/src/SMS/AcceptOrderSMS.php
@@ -11,15 +11,16 @@ class AcceptOrderSMS extends SMS
 
     public function __construct($order)
     {
-        $this->recipient = '993'.json_decode($order)->customer_email;
+        Log::info(json_decode($order));
+        $this->recipient = '993'.$order->customer_email;
         $this->id = 'order_'.$order->id;
         $this->text = 'Sizin ozan.com.tm  #'.$order->id.' belgili sargydyňyz kabul edildi';//trans('shop::app.sms.verification',$data);
 
         Log::info('AcceptOrderSMS const');
         Log::info('recipient: '.$this->recipient);
         Log::info('order phone: '.$order->customer_email);
+        Log::info('order_id'.$this->id );
         Log::info($order);
-        Log::info($order['customer_email']);
 
     }
 
