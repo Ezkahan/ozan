b4a1eaacacd7fb9e0902b90a7aedc16fbb79eb09
merdiano <merdan.m@gmail.com>
2021-09-26 18:47:59 +0500

    product_brand

--- packages/Webkul/API/Http/Controllers/Shop/ProductController.php
+++ packages/Webkul/API/Http/Controllers/Shop/ProductController.php
@@ -58,7 +58,7 @@ class ProductController extends Controller
         $productResource =  ProductResource::make(
             $product
         );
-        $productResource->categories = $product->produt->categories;
+        $productResource->categories = $product->product->categories;
 
         $productResource->related_products = $product->related_products()->get();
 
