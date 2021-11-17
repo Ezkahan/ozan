098bde87ddc05d5743d0eae9bd1c34c1f5b9ae75
merdan <merdan.m@gmail.com>
2021-11-17 17:04:37 +0500

    find order error6

--- packages/Webkul/Admin/src/DataGrids/OrderDataGrid.php
+++ packages/Webkul/Admin/src/DataGrids/OrderDataGrid.php
@@ -23,6 +23,8 @@ class OrderDataGrid extends DataGrid
                 $leftJoin->on('order_address_billing.order_id', '=', 'orders.id')
                          ->where('order_address_billing.address_type', OrderAddress::ADDRESS_TYPE_BILLING);
             })
+            ->leftJoin('cart_payment as payment','payment.cart_id','=','orders.cart_id')
+            ->addSelect('payment.method')
             ->addSelect('orders.id','orders.increment_id', 'orders.base_sub_total', 'orders.base_grand_total', 'orders.created_at', 'channel_name', 'status')
             ->addSelect(DB::raw('CONCAT(' . DB::getTablePrefix() . 'order_address_billing.first_name, " ", ' . DB::getTablePrefix() . 'order_address_billing.last_name) as billed_to'))
             ->addSelect(DB::raw('CONCAT(' . DB::getTablePrefix() . 'order_address_shipping.first_name, " ", ' . DB::getTablePrefix() . 'order_address_shipping.last_name) as shipped_to'));
@@ -73,14 +75,14 @@ class OrderDataGrid extends DataGrid
             'filterable' => true,
         ]);
 
-//        $this->addColumn([
-//            'index'      => 'channel_name',
-//            'label'      => trans('admin::app.datagrid.channel-name'),
-//            'type'       => 'string',
-//            'sortable'   => true,
-//            'searchable' => true,
-//            'filterable' => true,
-//        ]);
+        $this->addColumn([
+            'index'      => 'method',
+            'label'      => trans('admin::app.app.sales.orders.payment-method'),
+            'type'       => 'string',
+            'sortable'   => false,
+            'searchable' => false,
+            'filterable' => false,
+        ]);
 
         $this->addColumn([
             'index'      => 'status',
