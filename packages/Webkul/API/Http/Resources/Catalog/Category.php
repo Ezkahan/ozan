be9ff3b8ed8f292d4f955e9bc984742afb27f9f2
IamSerdar <programmer.asa@gmail.com>
2022-12-15 09:10:01 +0500

    test commit

--- packages/Webkul/API/Http/Resources/Catalog/Category.php
+++ packages/Webkul/API/Http/Resources/Catalog/Category.php
@@ -3,9 +3,8 @@
 namespace Webkul\API\Http\Resources\Catalog;
 
 use Illuminate\Http\Resources\Json\JsonResource;
-use Illuminate\Support\Facades\DB;
 use Webkul\API\Http\Resources\Catalog\Product as ProductResource;
-use Webkul\Product\Repositories\ProductFlatRepository;
+use Webkul\Product\Models\Product;
 
 class Category extends JsonResource
 {
@@ -40,51 +39,16 @@ class Category extends JsonResource
         $categoryId = $this->id;
         if ($request->has('include')) {
 
-            $params = request()->input();
-
-        $repository = app(ProductFlatRepository::class)->scopeQuery(function ($query) use ($params, $categoryId) {
-            $channel = request()->get('channel') ?: (core()->getCurrentChannelCode() ?: core()->getDefaultChannelCode());
-
-            $locale = request()->get('locale') ?: app()->getLocale();
-
-            $qb = $query->distinct()
-                ->select('product_flat.*')
-                ->join('product_flat as variants', 'product_flat.id', '=', DB::raw('COALESCE(' . DB::getTablePrefix() . 'variants.parent_id, ' . DB::getTablePrefix() . 'variants.id)'))
-                ->leftJoin('product_categories', 'product_categories.product_id', '=', 'product_flat.product_id')
-                ->leftJoin('product_attribute_values', 'product_attribute_values.product_id', '=', 'variants.product_id')
-                ->where('product_flat.channel', $channel)
-                ->where('product_flat.locale', $locale)
-                ->whereNotNull('product_flat.url_key');
-
-            if ($categoryId) {
-                $qb->where('product_categories.category_id', $categoryId);
-            }
-
-            if (is_null(request()->input('status'))) {
-                $qb->where('product_flat.status', 1);
-            }
-
-            if (is_null(request()->input('visible_individually'))) {
-                $qb->where('product_flat.visible_individually', 1);
-            }
-
-            return $qb->groupBy('product_flat.id');
-
-        });
-
-            $result['products'] = ProductResource::collection(
-                $repository->limit(4)->get()
-            //     Product::
-            // whereHas('inventories', function ($query){
-            //     $query->where('qty', '>', 0);
-            // })->whereHas('product_flat', function ($query){
-            //     $query->where('status', 1);
-            // })->whereHas('categories', function ($query) use ($categoryId){
-            //     $query->where('category_id', $categoryId);
-            // })
-            // ->where('status', true)
-            // ->inRandomOrder()
-            // ->limit(4)->get()
+            $result['products'] = ProductResource::collection(Product::whereHas('inventories', function ($query){
+                $query->where('qty', '>', 0);
+            })->whereHas('product_flat', function ($query){
+                $query->where('status', 1);
+            })->whereHas('categories', function ($query) use ($categoryId){
+                $query->where('category_id', $categoryId);
+            })
+            ->where('status', true)
+            ->inRandomOrder()
+            ->limit(4)->get()
         );
         }
 
