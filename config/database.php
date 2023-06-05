c64f9986a6bb17bdc048add9578622beba4e6704
merdan <merdan.m@gmail.com>
2023-06-05 15:59:57 +0500

    cache to routes

--- config/database.php
+++ config/database.php
@@ -115,6 +115,20 @@ return [
             'database' => 0,
         ],
 
+        'cache' => [
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_CACHE_DATABASE', '1'),
+        ],
+
+        'session' => [
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_SESSION_DATABASE', '2'),
+        ],
+
     ],
 
 ];
