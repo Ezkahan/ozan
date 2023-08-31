8a7dd5e19bb298d2df5f25fa0128b505918c9460
ezkahan <ezkahan.dev@gmail.com>
2023-08-31 17:41:46 +0500

    customer login with sms verification. customer guard attempt by phone

--- packages/Webkul/Customer/src/Http/Controllers/SessionController.php
+++ packages/Webkul/Customer/src/Http/Controllers/SessionController.php
@@ -73,9 +73,8 @@ class SessionController extends Controller
 
         if ($customer && request('sms_code')) {
             if ($customer->sms_code == request('sms_code')) {
-                auth()->guard('customer')->loginUsingId($customer->id);
+                auth()->guard('customer')->attempt(['phone' => $phone]);
                 // auth()->guard('customer')->login($customer);
-
                 //Event passed to prepare cart after login
                 Event::dispatch('customer.after.login', request('phone'));
 
