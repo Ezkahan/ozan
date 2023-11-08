7a0bb68c6341368dbbc3ced99e54b8f3fe71f266
ezkahan <ezkahan.dev@gmail.com>
2023-11-08 16:21:44 +0500

    product stock checking 2

--- packages/Webkul/Shop/src/Resources/views/products/view/stock.blade.php
+++ packages/Webkul/Shop/src/Resources/views/products/view/stock.blade.php
@@ -1,9 +1,9 @@
 {!! view_render_event('bagisto.shop.products.view.stock.before', ['product' => $product]) !!}
 
-<div class="stock-status {{! $product->haveSufficientQuantity(1) ? '' : 'active' }}">
-    @if ( $product->haveSufficientQuantity(1) === true )
+<div class="stock-status {{ !$product->haveSufficientQuantity(1) ? '' : 'active' }}">
+    @if ($product->haveSufficientQuantity(1) === true)
         {{ __('shop::app.products.in-stock') }}
-    @elseif ( $product->haveSufficientQuantity(1) > 0 )
+    @elseif ($product->haveSufficientQuantity(1) > 0)
         {{ __('shop::app.products.available-for-order') }}
     @else
         {{ __('shop::app.products.out-of-stock') }}
--- resources/themes/velocity/views/products/view/stock.blade.php
+++ resources/themes/velocity/views/products/view/stock.blade.php
@@ -1,16 +1,15 @@
 {!! view_render_event('bagisto.shop.products.view.stock.before', ['product' => $product]) !!}
 
 <div class="col-12 availability">
-    <button
-        type="button"
-        class="{{! $product->haveSufficientQuantity(1) ? '' : 'active' }} disable-box-shadow">
-            @if ( $product->haveSufficientQuantity(1) === true )
-                {{ __('shop::app.products.in-stock') }}
-            @elseif ( $product->haveSufficientQuantity(1) > 0 )
-                {{ __('shop::app.products.available-for-order') }}
-            @else
-                {{ __('shop::app.products.out-of-stock') }}
-            @endif
+    <button type="button"
+        class="{{ !$product->haveSufficientQuantity($product->totalQuantity()) ? '' : 'active' }} disable-box-shadow">
+        @if ($product->haveSufficientQuantity($product->totalQuantity()) === true)
+            {{ __('shop::app.products.in-stock') }}
+        @elseif ($product->haveSufficientQuantity($product->totalQuantity()) > 0)
+            {{ __('shop::app.products.available-for-order') }}
+        @else
+            {{ __('shop::app.products.out-of-stock') }}
+        @endif
     </button>
 </div>
 
