b2483a84e648b6d9ac6c6f27c6c0a82cd2e1e79a
ezkahan <ezkahan.dev@gmail.com>
2023-11-08 16:31:05 +0500

    product stock checking 3

--- resources/themes/velocity_ozan/views/products/view/stock.blade.php
+++ resources/themes/velocity_ozan/views/products/view/stock.blade.php
@@ -1,6 +1,7 @@
 {!! view_render_event('bagisto.shop.products.view.stock.before', ['product' => $product]) !!}
 
 <div class="col-12 availability">
+    {{ dd($product) }}
     <button type="button"
         class="{{ !$product->haveSufficientQuantity($product->totalQuantity()) ? '' : 'active' }} disable-box-shadow">
         @if ($product->haveSufficientQuantity($product->totalQuantity()) === true)
