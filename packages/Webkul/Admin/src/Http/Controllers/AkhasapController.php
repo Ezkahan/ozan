4b5755811289b0731a21226552ae28a2b4e59fe6
ezkahan <ezkahan.dev@gmail.com>
2023-11-08 15:18:41 +0500

    akhasap controller

--- packages/Webkul/Admin/src/Http/Controllers/AkhasapController.php
+++ packages/Webkul/Admin/src/Http/Controllers/AkhasapController.php
@@ -4,12 +4,18 @@ namespace Webkul\Admin\Http\Controllers;
 
 use Log;
 use Illuminate\Http\Request;
+use Webkul\Product\Models\Product;
+use Webkul\Product\Models\ProductInventory;
 
 class AkhasapController extends Controller
 {
     public function sync(Request $request)
     {
-        Log::debug($request->all());
-        return $request->all();
+        $product = Product::where('sku', 'kn00003590')->get();
+        Log::debug('Total before: ' . $product->totalQuantity());
+        ProductInventory::where('product_id', $product->id)->update(['qty' => 100]);
+        Log::debug('Total after: ' . $product->totalQuantity());
+        // Log::debug($request->all());
+        // return $request->all();
     }
 }
