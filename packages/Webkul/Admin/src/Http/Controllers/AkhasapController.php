46e02e37cc166a55afc2dec45d116405e2ed1f9b
ezkahan <ezkahan.dev@gmail.com>
2023-11-10 12:58:37 +0500

    log test

--- packages/Webkul/Admin/src/Http/Controllers/AkhasapController.php
+++ packages/Webkul/Admin/src/Http/Controllers/AkhasapController.php
@@ -12,18 +12,21 @@ class AkhasapController extends Controller
 {
     public function sync(Request $request)
     {
+        Log::info('_____________');
+        Log::debug($request->all());
+        Log::info('_____________');
         // "sku": "J200000696",
         // "stock": "-9.000000",
         // "barcode": "01080",
         // "sale_price": "2.60000"
 
-        $product = Product::where('sku', 'J200000696')->first();
-        // Log::info($product);
-        Log::debug('Total before: ' . $product->totalQuantity());
-        // ProductInventory::where('product_id', $product->id)->update(['qty' => 100]);
-        // Log::debug('Total after: ' . $product->totalQuantity());
-        $productFlat = ProductFlat::where('sku', 'J200000696')->get();
-        Log::info($productFlat);
+        // $product = Product::where('sku', 'J200000696')->first();
+        // // Log::info($product);
+        // Log::debug('Total before: ' . $product->totalQuantity());
+        // // ProductInventory::where('product_id', $product->id)->update(['qty' => 100]);
+        // // Log::debug('Total after: ' . $product->totalQuantity());
+        // $productFlat = ProductFlat::where('sku', 'J200000696')->get();
+        // Log::info($productFlat);
 
         // Log::debug($request->all());
         // return $request->all();
