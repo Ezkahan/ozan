7e26dbc80a4d706c683f0b75fb964109067fed04
Yusup2001 <yusupgurbanmammedow2001@gmail.com>
2024-06-11 16:30:33 +0500

    Product edit brand

--- packages/Webkul/Admin/src/Resources/views/catalog/products/field-types/select.blade.php
+++ packages/Webkul/Admin/src/Resources/views/catalog/products/field-types/select.blade.php
@@ -1,21 +1,27 @@
-<select v-validate="'{{$validations}}'" class="control" id="{{ $attribute->code }}" name="{{ $attribute->code }}" data-vv-as="&quot;{{ $attribute->admin_name }}&quot;">
+<select v-validate="'{{ $validations }}'" class="control" id="{{ $attribute->code }}" name="{{ $attribute->code }}"
+    data-vv-as="&quot;{{ $attribute->admin_name }}&quot;">
 
-    <?php $selectedOption = old($attribute->code) ?: $product[$attribute->code] ?>
+    <?php $selectedOption = old($attribute->code) ?: $product[$attribute->code]; ?>
 
     @if ($attribute->code != 'tax_category_id')
-
-        @foreach ($attribute->options()->orderBy('sort_order')->get() as $option)
-            <option value="{{ $option->id }}" {{ $option->id == $selectedOption ? 'selected' : ''}}>
-                {{ $option->admin_name }}
-            </option>
-        @endforeach
-
+        @if ($attribute->code == 'brand')
+            @foreach ($attribute->options()->orderBy('admin_name')->get() as $option)
+                <option value="{{ $option->id }}" {{ $option->id == $selectedOption ? 'selected' : '' }}>
+                    {{ $option->admin_name }}
+                </option>
+            @endforeach
+        @else
+            @foreach ($attribute->options()->orderBy('sort_order')->get() as $option)
+                <option value="{{ $option->id }}" {{ $option->id == $selectedOption ? 'selected' : '' }}>
+                    {{ $option->admin_name }}
+                </option>
+            @endforeach
+        @endif
     @else
-
         <option value=""></option>
 
         @foreach (app('Webkul\Tax\Repositories\TaxCategoryRepository')->all() as $taxCategory)
-            <option value="{{ $taxCategory->id }}" {{ $taxCategory->id == $selectedOption ? 'selected' : ''}}>
+            <option value="{{ $taxCategory->id }}" {{ $taxCategory->id == $selectedOption ? 'selected' : '' }}>
                 {{ $taxCategory->name }}
             </option>
         @endforeach
